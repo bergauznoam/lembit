23e871aae4e17676b42c151f71f9eee0d8561527
Noam Bergauz <bergauznoam@gmail.com>
2023-06-23 18:50:35 +0300

    Added subscribed communities list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,9 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { getClient } from "@lemmy";
-import { GetPost, GetCommunity } from "lemmy-js-client";
+import { LemmyHttp, ListCommunities, ListCommunitiesResponse } from "lemmy-js-client";
+import { Account, DatabaseService } from '@services/database.service';
+import { ICommunityItem } from "@interfaces/community-item.interface";
 
 @Component({
   selector: 'app-root',
@@ -14,7 +16,15 @@ import { GetPost, GetCommunity } from "lemmy-js-client";
   imports: [IonicModule, RouterLink, RouterLinkActive, CommonModule],
 })
 export class AppComponent implements OnInit {
-  public appPages = [
+  private authToken!: string | null;
+  private lemmyClient!: LemmyHttp;
+  public account!: Account;
+
+  constructor(
+    private readonly databaseService: DatabaseService
+  ) {}
+
+  public communities: ICommunityItem[] = [
     { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
     { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
     { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
@@ -24,6 +34,28 @@ export class AppComponent implements OnInit {
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   public async ngOnInit(): Promise<void> {
-
+    this.authToken = localStorage.getItem("authToken");
+    const account = await this.databaseService.getPrimaryAccount();
+    if (account) {
+      this.account = account;
+    }
+    this.lemmyClient = getClient(this.account?.server || "lemmy.world");
+    const request: ListCommunities = {
+      auth: this.authToken || undefined,
+      type_: this.authToken ? "Subscribed" : "Local",
+      limit: 50
+    }
+    const response: ListCommunitiesResponse = await this.lemmyClient.listCommunities(request)
+    this.communities = response.communities
+      .filter(community => 
+          !community.blocked && 
+          !community.community.deleted && 
+          !community.community.hidden && 
+          !community.community.removed)
+      .map(community => ({
+        title: community.community.name,
+        url: `/community/${community.community.id}`,
+        icon: community.community.icon
+      }))
   }
 }
---
+++ src/app/interfaces/community-item.interface.ts
@@ -0,0 +1,5 @@
+export interface ICommunityItem {
+    readonly title: string;
+    readonly url: string;
+    readonly icon?: string;
+}
